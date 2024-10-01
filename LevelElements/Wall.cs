d6f18ba48b1ebf9836a4f239629aa230f09c4e83
hoarseProgramming <hampus.es@gmail.com>
2024-10-01 22:24:50 +0200

    add Wall class

---
+++ LevelElements/Wall.cs
@@ -0,0 +1,21 @@
+﻿//Dungeon Crawler.
+
+
+//Game Loop
+class Wall : LevelElement
+{
+    public Wall(Position position)
+    {
+        Sprite = '#';
+        SpriteColor = ConsoleColor.White;
+        Position = position;
+    }
+    public override void UpdateIsInsideVisionRange(Hero hero)
+    {
+        double distanceBetweenPositions = Position.CalculateDistanceBetweenPositions(hero.Position, Position);
+        if (distanceBetweenPositions <= hero.VisionRange)
+        {
+            IsInsideVisionRange = true;
+        }
+    }
+}
