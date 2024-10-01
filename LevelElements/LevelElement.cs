3b09a25a38014c054847ca5140b0c48a537f25f6
hoarseProgramming <hampus.es@gmail.com>
2024-10-01 22:24:42 +0200

    add abstract class Levelelement with methods for printing and updating visibility

---
+++ LevelElements/LevelElement.cs
@@ -0,0 +1,39 @@
+﻿
+abstract class LevelElement
+{
+    protected bool _isInsideVisionRange = false;
+    public bool IsInsideVisionRange
+    {
+        get { return _isInsideVisionRange; }
+        set { _isInsideVisionRange = value; }     
+    }
+    public Position Position { get; set; }
+    public Char Sprite { get; set; }
+    public ConsoleColor SpriteColor { get; set;  }
+    public void Draw(bool isInsideVisionRange)
+    {
+        Console.SetCursorPosition(Position.X, Position.Y);
+
+        if (isInsideVisionRange)
+        {
+            Console.ForegroundColor = SpriteColor;
+            Console.Write(Sprite);
+        }
+        else
+        {
+            Console.Write(' ');
+        }
+    }
+    public virtual void UpdateIsInsideVisionRange(Hero hero)
+    {
+        double distanceBetweenPositions = Position.CalculateDistanceBetweenPositions(hero.Position, Position);
+        if (distanceBetweenPositions <= hero.VisionRange)
+        {
+            IsInsideVisionRange = true;
+        }
+        else
+        {
+            IsInsideVisionRange = false;
+        }
+    }
+}
