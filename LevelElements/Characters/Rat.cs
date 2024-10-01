95ae47b6db1ca7bcea5e8ed4761dfc44a69d8cef
hoarseProgramming <hampus.es@gmail.com>
2024-10-01 22:23:30 +0200

    add Rat class with movement and base stats

---
+++ LevelElements/Characters/Rat.cs
@@ -0,0 +1,57 @@
+﻿
+class Rat : Enemy
+{
+    public void Move(List<LevelElement> currentLevel)
+    {
+        Position potentialPosition = GetPotentialPosition();
+
+        LevelElement elementCollidedWith = CheckCollision(currentLevel, potentialPosition);
+
+        if (elementCollidedWith is Hero)
+        {
+            Console.WriteLine("Attack!");
+        }
+        else if (elementCollidedWith is Enemy || elementCollidedWith is Wall) { }
+        else
+        {
+            Console.SetCursorPosition(Position.X, Position.Y);
+            Console.Write(' ');
+            Position = potentialPosition;
+        }
+    }
+    public Position GetPotentialPosition()
+    {
+        Random r = new Random();
+        Direction direction = (Direction)r.Next(4);
+        if (direction == Direction.UP)
+        {
+            return new Position(Position.X, Position.Y - 1);
+        }
+        else if (direction == Direction.DOWN)
+        {
+            return new Position(Position.X, Position.Y + 1);
+        }
+        else if (direction == Direction.LEFT)
+        {
+            return new Position(Position.X - 1, Position.Y);
+        }
+        else
+        {
+            return new Position(Position.X + 1, Position.Y);
+        }
+    }
+    public override void Update()
+    {
+        throw new NotImplementedException();
+    }
+    public Rat(Position position)
+    {
+        Sprite = 'r';
+        SpriteColor = ConsoleColor.Red;
+        Position = position;
+        Name = "rat";
+        HP = 10;
+        AttackDice = new Dice(1, 6, 3);
+        DefenceDice = new Dice(1, 6, 1);
+    }
+}
