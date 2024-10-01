f360fc0ee939e355f73bc0bd6ccb6b3993136f2a
hoarseProgramming <hampus.es@gmail.com>
2024-10-01 22:23:18 +0200

    add Hero class with movement and base stats

---
+++ LevelElements/Characters/Hero.cs
@@ -0,0 +1,57 @@
+﻿
+class Hero : Character
+{
+    public int VisionRange { get; set; }
+    public void Move(List<LevelElement> currentLevel)
+    {
+        Position potentialPosition = GetPotentialPosition();
+        
+        LevelElement elementCollidedWith = CheckCollision(currentLevel, potentialPosition);
+
+        if (elementCollidedWith is Enemy)
+        {
+            Console.WriteLine("Attack!");
+        }
+        else if (elementCollidedWith is Wall) {}
+        else
+        {
+            Console.SetCursorPosition(Position.X, Position.Y);
+            Console.Write(' ');
+            Position = potentialPosition;
+        }
+    }
+    public Position GetPotentialPosition()
+    {
+        ConsoleKeyInfo input = Console.ReadKey();
+
+        if (input.Key == ConsoleKey.UpArrow)
+        {
+            return Position.GetPosition(Direction.UP);
+        }
+        else if (input.Key == ConsoleKey.DownArrow)
+        {
+            return Position.GetPosition(Direction.DOWN);
+        }
+        else if (input.Key == ConsoleKey.LeftArrow)
+        {
+            return Position.GetPosition(Direction.LEFT);
+        }
+        else if (input.Key == ConsoleKey.RightArrow)
+        {
+            return Position.GetPosition(Direction.RIGHT);
+        }
+        return Position;
+    }
+
+    public Hero(Position position, string name = "Hampus")
+    {
+        Sprite = '@';
+        SpriteColor = ConsoleColor.Yellow;
+        Position = position;
+        Name = name;
+        HP = 100;
+        AttackDice = new Dice(2, 6, 2);
+        DefenceDice = new Dice(2, 6, 0);
+        VisionRange = 5;
+    }
+}
