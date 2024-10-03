92d25e66f0fcc286ac9a27a3eb22e5fc5fcc3702
hoarseProgramming <hampus.es@gmail.com>
2024-10-03 15:44:29 +0200

    Cleaned up voicelines

--- LevelElements/Characters/Character.cs
+++ LevelElements/Characters/Character.cs
@@ -1,7 +1,4 @@
 ﻿
-
-using System.ComponentModel;
-
 abstract class Character : LevelElement
 {
     public string Name { get; set; }
@@ -33,49 +30,55 @@ abstract class Character : LevelElement
     }
     public void PrintVoiceLine(Character attacker, Character opponent, int attack, int defence, int damage)
     {
-        Console.ForegroundColor = ConsoleColor.Yellow;
+        string heroIsAttacking = $"You (ATK: {AttackDice} => {attack}) attacked the {opponent.Name} " +
+                    $"(Def: {opponent.DefenceDice} => {defence}) for {damage} damage";
+        string enemyIsAttacking = $"The {Name} (ATK: {AttackDice} => {attack}) attacked you " +
+                    $"(Def: {DefenceDice} => {defence}) for {damage} damage";
+
+
         if (attacker is Hero)
         {
-            if (damage == 0)
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            if (opponent.HP <= 0)
             {
-                Console.WriteLine($"You (ATK: {AttackDice} => {attack}) attacked the {((opponent is Rat) ? "rat" : "snake")} " +
-                    $"(Def: {opponent.DefenceDice} => {defence}), not even giving it a scratch.");
+                Console.WriteLine($"{heroIsAttacking}, sending it to a gruesome death.".PadRight(120));
+            }
+            else if (damage == 0)
+            {
+                Console.WriteLine($"{heroIsAttacking}, not even giving it a scratch.".PadRight(120));
             }
             else if (damage < 5)
             {
-                Console.WriteLine($"You (ATK: {AttackDice} => {attack}) attacked the {((opponent is Rat) ? "rat" : "snake")} " +
-                    $"(Def: {opponent.DefenceDice} => {defence}), giving it a small wound.");
+                Console.WriteLine($"{heroIsAttacking}, giving it a small wound.".PadRight(120));
             }
             else
             {
-                Console.WriteLine($"You (ATK: {AttackDice} => {attack}) attacked the {((opponent is Rat) ? "rat" : "snake")} " +
-                    $"(Def: {opponent.DefenceDice} => {defence}), severely wounding it!");
+                Console.WriteLine($"{heroIsAttacking}, severely wounding it!".PadRight(120));
             }
         }
         else
         {
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = this.SpriteColor;
+            if (opponent.HP <= 0)
+            {
+                Console.WriteLine($"{enemyIsAttacking}! You die.".PadRight(120));
+            }
             if (damage == 0)
             {
-                Console.WriteLine($"The {this} (ATK: {AttackDice} => {attack}) attacked you " +
-                    $"(Def: {DefenceDice} => {defence}) but missed terribly. \"What a fool!\"");
+                Console.WriteLine($"{enemyIsAttacking}! {opponent.Name}: \"What a fool!\"".PadRight(120));
             }
             else if (damage < 5)
             {
-                Console.WriteLine($"The {this} (ATK: {AttackDice} => {attack}) attacked you " +
-                    $"(Def: {DefenceDice} => {defence}). \"Bah! It's only a fleshwound!\"");
+                Console.WriteLine($"{enemyIsAttacking}! {opponent.Name}: \"Bah! It's only a fleshwound!\"".PadRight(120));
             }
             else
             {
-                Console.WriteLine($"The {this} (ATK: {AttackDice} => {attack}) attacked you " +
-                    $"(Def: {DefenceDice} => {defence}). \"Hmph! You're making me angry\"");
+                Console.WriteLine($"{enemyIsAttacking}! {opponent.Name}: \"Hmph! You're making me angry\"".PadRight(120));
+                
             }
         }
-
     }
 
-    //public abstract void Move(List<LevelElement> currentLevel);
-    //public abstract Position GetPotentialPosition();
     public LevelElement CheckCollision(List<LevelElement> currentLevel, Position potentialPosition)
     {
         foreach (LevelElement element in currentLevel)
