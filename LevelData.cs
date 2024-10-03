267c6b334cba26add21422c79b631ebe9bdc3e62
hoarseProgramming <hampus.es@gmail.com>
2024-10-03 15:46:46 +0200

    update playing field position

--- LevelData.cs
+++ LevelData.cs
@@ -11,26 +11,26 @@ class LevelData
 
         using (StreamReader reader = new StreamReader(pathToLevel))
         { 
-            Position readerPosition = new Position(0, 0);
+            Position readerPosition = new Position(0, 3);
             while (!reader.EndOfStream)
             {
                 char currentChar = (char)reader.Read();
                 if (currentChar == '#')
                 {
-                    _elements.Add(new Wall(new Position(readerPosition.X, readerPosition.Y + 4)));
+                    _elements.Add(new Wall(new Position(readerPosition.X, readerPosition.Y)));
                 }
                 if (currentChar == '@')
                 {
-                    _elements.Add(new Hero(new Position(readerPosition.X, readerPosition.Y + 4)));
+                    _elements.Add(new Hero(new Position(readerPosition.X, readerPosition.Y)));
                     Hero = (Hero)Elements[Elements.Count - 1];
                 }
                 else if (currentChar == 'r')
                 {
-                    _elements.Add(new Rat(new Position(readerPosition.X, readerPosition.Y + 4)));
+                    _elements.Add(new Rat(new Position(readerPosition.X, readerPosition.Y)));
                 }
                 else if (currentChar == 's')
                 {
-                    _elements.Add(new Snake(new Position(readerPosition.X, readerPosition.Y + 4)));
+                    _elements.Add(new Snake(new Position(readerPosition.X, readerPosition.Y)));
                 }
                 else if (currentChar == '\n')
                 {
@@ -42,6 +42,12 @@ class LevelData
             }
         }
 
+    }
+    public void PrintStatusBar()
+    {
+        Console.SetCursorPosition(0, 0);
+        Console.ForegroundColor = Hero.SpriteColor;
+        Console.WriteLine($"Name: {Hero.Name}  -  Health: {$"{Hero.HP}/100".PadLeft(7)}  -  Turn: {$"{Hero.Turn}".PadLeft(5)}");
     }
     public void DrawLevel()
     {
