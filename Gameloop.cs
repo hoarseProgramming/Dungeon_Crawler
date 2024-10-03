b8e7a1cfb6e1974460488dab3d762809ad88b533
hoarseProgramming <hampus.es@gmail.com>
2024-10-03 15:46:11 +0200

    Add statusbar, add death mechanic

---
+++ Gameloop.cs
@@ -0,0 +1,15 @@
+﻿//Dungeon Crawler.
+Console.CursorVisible = false;
+LevelData levelOne = new LevelData();
+levelOne.Load("Level1.txt");
+
+
+//Game Loop
+while (levelOne.Hero.IsAlive)
+{
+    levelOne.PrintStatusBar();
+    levelOne.UpdateVision();
+    levelOne.DrawLevel();
+    levelOne.MoveCharacters();
+    
+}
