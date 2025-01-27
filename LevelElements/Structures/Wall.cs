7a795beda1f59b0a88eca1a597d1ffa4bc957aea
hoarseProgramming <hampus.es@gmail.com>
2025-01-27 09:56:28 +0100

    Moved wall class

---
+++ LevelElements/Structures/Wall.cs
@@ -0,0 +1,21 @@
+﻿using Dungeon_Crawler.GameMacro;
+using Dungeon_Crawler.LevelElements.Structures;
+
+class Wall : Structure
+{
+    public Wall(Position position, Game game)
+    {
+        Sprite = '#';
+        SpriteColor = ConsoleColor.White;
+        Position = position;
+        Game = game;
+    }
+    //public override void UpdateIsInsideVisionRange(Hero hero)
+    //{
+    //    double distanceBetweenPositions = Position.CalculateDistanceBetweenPositions(hero.Position, Position);
+    //    if (distanceBetweenPositions <= hero.VisionRange)
+    //    {
+    //        IsInsideVisionRange = true;
+    //    }
+    //}
+}
