ddfc0096dd6e05075ba4ae1132d9ac37c8b043d1
hoarseProgramming <hampus.es@gmail.com>
2025-01-27 09:56:12 +0100

    Add door class

---
+++ LevelElements/Structures/Door.cs
@@ -0,0 +1,25 @@
+﻿using Dungeon_Crawler.GameMacro;
+
+namespace Dungeon_Crawler.LevelElements.Structures;
+
+internal class Door : Structure
+{
+    bool IsOpen = false;
+    bool IsLocked;
+    public Door(Position position, Game game, char sprite, bool isLocked = false)
+    {
+        Position = position;
+        Game = game;
+        Sprite = sprite;
+        SpriteColor = ConsoleColor.White;
+        IsLocked = isLocked;
+
+        if (sprite == '_') IsOpen = true;
+    }
+    public void OpenDoor()
+    {
+        IsOpen = true;
+        Sprite = '_';
+        Draw();
+    }
+}
