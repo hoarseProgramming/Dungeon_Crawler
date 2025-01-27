0c10274ed6057da5cc23a47eac027cbc76c7571b
hoarseProgramming <hampus.es@gmail.com>
2025-01-27 09:56:18 +0100

    add structure class

---
+++ LevelElements/Structures/Structure.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Dungeon_Crawler.LevelElements.Structures;
+[BsonKnownTypes(typeof(Wall), typeof(Door))]
+
+internal class Structure : LevelElement
+{
+    public override void UpdateIsInsideVisionRange(Hero hero)
+    {
+        double distanceBetweenPositions = Position.CalculateDistanceBetweenPositions(hero.Position, Position);
+        if (distanceBetweenPositions <= hero.VisionRange)
+        {
+            IsInsideVisionRange = true;
+        }
+    }
+}
+
